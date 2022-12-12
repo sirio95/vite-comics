6d1593c72ab6e24d142cbfc0b1e52e42a1851492
sirio95 <sirio.iasevoli@gmail.com>
2022-12-12 18:59:46 +0100

    header - raw(no scss)

---
+++ src/components/header/Hero.vue
@@ -0,0 +1,30 @@
+<script>
+    export default{
+        data(){
+            return{
+                pages: [
+                    "characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop" 
+                ]
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<template>
+    <div id="header">
+        <a href="#" id="logo">
+            <img src= "../../assets/img/dc-logo.png" alt="">
+        </a>
+        <ul v-for= "element in pages" :key="element" id="nav">
+            <li class="nav_item"><a href="">{{element}}</a></li>
+        </ul>
+    </div>
+    
+</template>
+
+<style>
+
+</style>
